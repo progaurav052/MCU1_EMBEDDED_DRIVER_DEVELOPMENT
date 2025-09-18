eb3b00fa7145e34e940efd8dd88e3c1434079087
progaurav052 <gpprojects511@gmail.com>
2025-09-18 22:40:27 +0530

    Created an drivers folder to for Driver Layer > Inc folder to store MCU device specific header file , Defined Macros for Various Bus Peripherals

---
+++ stm32f4xx_drivers/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ D:/GAURAV_G_PAI/Embedded-c/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+D:/GAURAV_G_PAI/Embedded-c/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
