ef4f56a668ec535bfaad8d2cd094141d57fcffc6
progaurav052 <gpprojects511@gmail.com>
2025-09-21 12:22:29 +0530

    Implemented User configurable Macros for GPIO pinNumber , pinMode , pupd ,altfn mode .... Implemented Init API

--- stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h
@@ -10,11 +10,13 @@
 
 #include "stm32f407xx.h"
 
-/* structure to config various modes , initial config*/
+/* structure to configuration various modes , initial configuration*/
+// user application should create an Handle structure and send it to driver , driver from this configuration  will init the Peripheral
+
 typedef struct{
 	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;
-	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinMode;   /*!< Possible values from @GPIO_PIN_MODES>*/
+	uint8_t GPIO_PinSpeed;   /*!< Possible values from @GPIO_SPEED_*>*/
 	uint8_t GPIO_PinPuPdControl;
 	uint8_t GPIO_PinOPType;
 	uint8_t GPIO_PinAltFunMode;
@@ -29,8 +31,56 @@ typedef struct{
 
 
 }GPIO_Handle_t;
-
 // this above structure will be provided by the driver API to the applications to change the config
+
+/*GPIO pin Number possible values*/
+
+#define GPIO_PIN_NO_0           0
+#define GPIO_PIN_NO_1           1
+#define GPIO_PIN_NO_2           2
+#define GPIO_PIN_NO_3           3
+#define GPIO_PIN_NO_4           4
+#define GPIO_PIN_NO_5           5
+#define GPIO_PIN_NO_6           6
+#define GPIO_PIN_NO_7           7
+#define GPIO_PIN_NO_8           8
+#define GPIO_PIN_NO_9           9
+#define GPIO_PIN_NO_10          10
+#define GPIO_PIN_NO_11          11
+#define GPIO_PIN_NO_12          12
+#define GPIO_PIN_NO_13          13
+#define GPIO_PIN_NO_14          14
+#define GPIO_PIN_NO_15          15
+
+
+/* GPIO Pin Possible Modes */
+#define GPIO_MODE_IN  			0
+#define GPIO_MODE_OUT   		1
+#define GPIO_MODE_ALTFN 		2
+#define GPIO_MODE_ANALOG 		3
+// we also have to configure for the Interrupt Modes such as falling edge , rising edge , etc ...
+#define GPIO_MODE_IT_FE			4
+#define GPIO_MODE_IT_RE 		5
+#define GPIO_MODE_IT_RFE		6
+
+
+
+/*GPIO Pin Possible output Types*/
+#define GPIO_OP_TYPE_PP			0
+#define GPIO_OP_TYPE_OD			1
+
+
+/*GPIO pin possible output speed modes*/
+#define GPIO_SPEED_LOW  			0
+#define GPIO_SPEED_MEDIUM   		1
+#define GPIO_SPEED_FAST 			2
+#define GPIO_SPEED_HIGH				3
+
+
+/*GPIO pin pull up pull down configuration  */
+#define GPIO_NO_PUPD				0
+#define GPIO_PIN_PU					1
+#define GPIO_PIN_PD                 2
 
 
 /******DRIVER API prototype****************/
