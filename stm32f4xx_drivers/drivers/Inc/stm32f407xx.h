2496d529c796035d7aafafee75bbf0eede892980
progaurav052 <gpprojects511@gmail.com>
2026-01-27 00:34:57 +0530

    Coding I2C_init API ,CR1,OAR ,phase 1

--- stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
@@ -12,6 +12,10 @@
 #include <string.h>
 #define __vo                                volatile
 
+uint32_t  AHB_PS[8]={2,4,8,16,64,128,256,512};
+uint32_t  APB_PS[5]={2,4,8,16};
+
+
 /*Processor specific Details , get these details from cortex-m4 documentation */
 
 /*NVIC Registers ISER and ICER register addresses*/
--- stm32f4xx_drivers/drivers/Src/stm32f4xx_i2c_driver.c
+++ stm32f4xx_drivers/drivers/Src/stm32f4xx_i2c_driver.c
@@ -44,6 +44,102 @@ void I2C_PeripheralClockControl(I2C_RegDef_t *pI2Cx,uint8_t EnorDi){
 /*Init and De-Init*/
 void I2C_Init(I2C_Handle_t *pI2CHandle){
 
+
+	uint32_t tempReg=0;
+
+	// configure the ACK bit in CR1 register
+	tempReg|=(pI2CHandle->I2C_Config.I2C_AckControl << I2C_CR1_ACK);
+    pI2CHandle->pI2Cx->I2C_CR1=tempReg;
+
+	//configure the SCL speed , for this calculation we need to know the PCLK speed (it can be anything based on preScaler)
+	//first configure the FREQ field in CR2 register
+    tempReg=0;
+    tempReg|=Cal_PCLK_Speed()/1000000;
+    pI2CHandle->pI2Cx->I2C_CR2=tempReg;
+
+
+    //program the Device own address
+    tempReg=0;
+    tempReg|=pI2CHandle->I2C_Config.I2C_DeviceAddress << 1;
+    tempReg|= (1<<14); //SPECIFIED IN MANUAL
+
+    pI2CHandle->pI2Cx->I2C_OAR1=tempReg;
+
+
+
+
+
+
+
+
+}
+
+uint32_t findClockSource()
+{
+	uint32_t temp;
+
+	temp = ((RCC->CFGR >> 2) & 0x3);
+
+	return temp;
+
+}
+uint32_t  Cal_PCLK_Speed()
+{
+	// clock source --> AHb1 prescaler --> APB1 prescaler ---> APB1 peripheral clocks
+
+	// find the clock source from RCC_CFGR Regsiter
+	uint32_t clckSource=0,SysClk=0,AHB_Prescaler=0, APB_Prescaler=0,temp=0,PCLK_SPEED;
+
+	clckSource=findClockSource();
+
+	if(clckSource==0)
+	{
+       sysClk=16000000;
+
+	}
+	else if(clckSource ==1)
+	{
+		sysClk=8000000;
+
+	}
+	else if(clckSource ==2)
+	{
+		sysClk=RCC_GetPLLOutputClock();
+
+	}
+
+	//need to find the preSclaer of AHB
+
+	temp = ((RCC->CFGR >> 4) &  0xF);
+	if(temp<8)
+	{
+		AHB_Prescaler=1;
+	}
+	else
+	{
+		AHB_Prescaler=AHB_PS[temp-8];
+	}
+
+
+	//need to find the Prescaler of APB
+	temp = ((RCC->CFGR >> 13) &  0x7);
+	if(temp < 4)
+	{
+		APB_Prescaler=1;
+	}
+	else
+	{
+		APB_Prescaler=APB_Prescaler[temp-4];
+	}
+
+
+	PCLK_SPEED=((SysClk/AHB_Prescaler)/APB_Prescaler);
+
+	return PCLK_SPEED;
+
+
+
+
 }
 void I2C_DeInit(I2C_RegDef_t *pI2Cx){
 
