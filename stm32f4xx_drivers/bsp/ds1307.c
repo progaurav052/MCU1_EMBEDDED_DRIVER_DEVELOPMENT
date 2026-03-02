f097eedefe89b7e0fb328847452291d864c11590
progaurav052 <gpprojects511@gmail.com>
2026-03-03 01:00:57 +0530

    coded set_time , set_date , get_time and get_date functions ... refer the timekeeper register in DS1307 Doc . data stored in BCD format in each register of 8 bit

--- stm32f4xx_drivers/bsp/ds1307.c
+++ stm32f4xx_drivers/bsp/ds1307.c
@@ -13,6 +13,8 @@ static void ds1307_i2c_pin_config(void);
 static void ds1307_i2c_config(void);
 static uint8_t ds1307_read(uint8_t reg_addr);
 static void ds1307_write(uint8_t value,uint8_t reg_addr);
+static uint8_t binary_to_bcd(uint8_t value);
+
 
 I2C_Handle_t g_ds1307I2cHandle;
 
@@ -45,16 +47,78 @@ uint8_t ds1307_init(void){
 
 void ds1307_set_current_time(RTC_Time_t *rtc_time){
 
+	//here we have to go to the individual sec , min , hrs register and set the value provided by user config in BCD format
+
+	//1. program the seconds field
+	uint8_t seconds = rtc_time->seconds;
+	//convert to bcd
+	seconds = binary_to_bcd(seconds);
+	//program this bcd format seconds in 00h register
+	//make sure the 8th bit is still  0 for the oscillator to work
+	seconds&=~(1 << 7);
+	ds1307_write(seconds, DS1307_ADDR_SEC);
+
+	//program the minutes
+	ds1307_write(binary_to_bcd(rtc_time->minutes),DS1307_ADDR_MIN);
+
+	//program the hour , 12 hr and 24 hr format
+	hrs = binary_to_bcd(rtc_time->hours);
+
+	if (rtc_time->time_format == TIME_FORMAT_24HRS) {
+		hrs &= ~(1 << 6);
+	} else {
+		hrs |= (1 << 6);
+		hrs = (rtc_time->time_format == TIME_FORMAT_12HRS_PM) ? hrs | (1 << 5) : hrs & ~(1 << 5);
+	}
+
+	ds1307_write(hrs, DS1307_ADDR_HRS);
+
+
 }
 void ds1307_get_current_time(RTC_Time_t *rtc_time){
 
+	// we need to fetch seconds , min , hours , time format and store it back in the data structure
+	uint8_t seconds,hrs;
+
+		seconds = ds1307_read(DS1307_ADDR_SEC);
+
+		seconds &= ~( 1 << 7);
+
+		rtc_time->seconds = bcd_to_binary(seconds);
+		rtc_time->minutes = bcd_to_binary(ds1307_read(DS1307_ADDR_MIN));
+
+		hrs = ds1307_read(DS1307_ADDR_HRS);
+		if(hrs & ( 1 << 6)){
+			//12 hr format
+			rtc_time->time_format =  (hrs & ( 1 << 5)) ? TIME_FORMAT_12HRS_PM  	: TIME_FORMAT_12HRS_AM ;
+			hrs &= ~(0x3 << 5);//Clear 6 and 5
+		}else{
+			//24 hr format
+			rtc_time->time_format = TIME_FORMAT_24HRS;
+		}
+
+		rtc_time->hours = bcd_to_binary(hrs);
+	}
 }
 
 void ds1307_set_current_date(RTC_Date_t *rtc_date){
 
+	ds1307_write(binary_to_bcd(rtc_date->date), DS1307_ADDR_DATE);
+
+	ds1307_write(binary_to_bcd(rtc_date->month), DS1307_ADDR_MONTH);
+
+	ds1307_write(binary_to_bcd(rtc_date->year), DS1307_ADDR_YEAR);
+
+	ds1307_write(binary_to_bcd(rtc_date->day), DS1307_ADDR_DAY);
+
+
 }
 void ds1307_get_current_date(RTC_Date_t *rtc_date){
 
+	rtc_date->day = bcd_to_binary(ds1307_read(DS1307_ADDR_DAY));
+	rtc_date->date = bcd_to_binary(ds1307_read(DS1307_ADDR_DATE));
+	rtc_date->month = bcd_to_binary(ds1307_read(DS1307_ADDR_MONTH));
+	rtc_date->year = bcd_to_binary(ds1307_read(DS1307_ADDR_YEAR));
 }
 
 static void ds1307_i2c_pin_config(void)
@@ -105,13 +169,13 @@ static uint8_t ds1307_read(uint8_t reg_addr){
 	// this function is done to read the value at particular reg_address of DS1307 chip
 	// firstly i am using it to verify if the value at 00h reg_address after making ch=0 has actually worked
 	// for this first make the internal address pointer point to the given address
-	uint8_t ch_state;
+	uint8_t data;
 	I2C_MasterSendData(&g_ds1307I2cHandle, &reg_addr, 1, DS1307_I2C_ADDRESS,0);
 
 	//receive
-	I2C_MasterReceiveData(&g_ds1307I2cHandle, &ch_state, 1, DS1307_I2C_ADDRESS,0);
+	I2C_MasterReceiveData(&g_ds1307I2cHandle, &data, 1, DS1307_I2C_ADDRESS,0);
 
-	return ch_state;
+	return data;
 
 
 }
@@ -125,3 +189,6 @@ static void ds1307_write(uint8_t value,uint8_t reg_addr){
 	I2C_MasterSendData(&g_ds1307I2cHandle, tx, 2, DS1307_I2C_ADDRESS, 0);
 }
 
+static uint8_t binary_to_bcd(uint8_t value){
+
+}
